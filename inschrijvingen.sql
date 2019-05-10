572e45e87cd7a19952a19e5bd7af1bb370059181
Mitchell Nooitgedacht <mitchell.n@outlook.com>
2019-05-10 12:54:08 +0200

    Update 4

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,3 @@
-
-INSERT INTO klassen_studenten (klas_code, ov_nummer) VALUES 
-	('90000','piet', 'jan', 1013), 
-    ('90001','henk', 'piet', 1014), 
-    ('90002','sla', 'tim', 1015), 
-    ('90003','komkommer', 'nick', 1016);
+UPDATE studenten 
+SET achternaam = 'komkommer', geboortedatum = '2000-01-01' 
+WHERE ov_nummer = 90003;
