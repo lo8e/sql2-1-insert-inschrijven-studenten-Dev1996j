ef5bd7f70684ba68b6d3b5eafbc1816123a2fb38
Mitchell Nooitgedacht <mitchell.n@outlook.com>
2019-05-10 12:33:37 +0200

    Update 1

---
+++ inschrijvingen.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES 
+	('lo9e-amo1','akr02', 2019, 'amo'), 
+    ('lo9e-amo2','vlt03', 2019, 'amo'), 
+    ('lo9e-amo3','bkr02', 2019, 'amo');
